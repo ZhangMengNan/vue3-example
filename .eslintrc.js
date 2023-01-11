47d00fbe44b8b76db2d457852c80b7c1c7e0ea9a
zhangmengnan <823421131@qq.com>
2023-01-11 19:10:50 +0800

    feat(deps): init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,12 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:vue/vue3-essential', 'standard-with-typescript'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: [
-    'vue'
-  ],
-  rules: {
-  }
+  plugins: ['vue'],
+  rules: {}
 }
